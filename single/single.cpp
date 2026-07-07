7a2710393371bcd05e47d9e08ad8663256a8b44a
4D-12\koyo <a>
2026-07-07 13:28:28 +0900

    全体のソースコードの入力

--- single/single.cpp
+++ single/single.cpp
@@ -1,1 +1,27 @@
-﻿
+﻿#include <iostream>
+
+class GameManager {
+private:
+
+	GameManager() {}
+
+public:
+
+	//　コピー・代入の禁止
+	GameManager(const GameManager&) = delete;
+	GameManager& operator = (const GameManager&) = delete;
+
+	static GameManager& Instance() {
+		static GameManager instance;
+		return instance;
+	}
+
+	void StartGame() {
+		// ゲーム開始処理
+	}
+};
+
+int main() {
+	//　使用例
+	GameManager::Instance().StartGame();
+}
